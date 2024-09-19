ffe8d122926e42ef1996f31d40742112c444a731
Olooce <asirastephen9@gmail.com>
2024-09-19 12:50:07 +0300

    Refactoring

--- src/main/java/ac/ku/oloo/fedhaApp.java
+++ src/main/java/ac/ku/oloo/fedhaApp.java
@@ -1,13 +1,15 @@
 package ac.ku.oloo;
 
+import ac.ku.oloo.configs.LoadConfig;
+
 /**
  * Default (Template) Project (org.example)
  * Created by: oloo
  * On: 19/09/2024. 11:43
  * Description:
  **/
-public class Main {
+public class fedhaApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LoadConfig Config = new LoadConfig();
     }
 }
