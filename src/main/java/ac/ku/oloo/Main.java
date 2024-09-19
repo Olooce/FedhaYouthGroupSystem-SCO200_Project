188ea57bee040945466947b8a67c1328fb3cbed6
Olooce <asirastephen9@gmail.com>
2024-09-19 11:48:30 +0300

    Initial commit

---
+++ src/main/java/ac/ku/oloo/Main.java
@@ -0,0 +1,13 @@
+package ac.ku.oloo;
+
+/**
+ * Default (Template) Project (org.example)
+ * Created by: oloo
+ * On: 19/09/2024. 11:43
+ * Description:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
