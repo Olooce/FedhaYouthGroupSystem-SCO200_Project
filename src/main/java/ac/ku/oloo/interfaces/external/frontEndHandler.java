ef0c3f05e335404a0bce19b67b14454b50cca760
Olooce <asirastephen9@gmail.com>
2024-09-19 19:50:09 +0300

    New content added

---
+++ src/main/java/ac/ku/oloo/interfaces/external/frontEndHandler.java
@@ -0,0 +1,10 @@
+package ac.ku.oloo.interfaces.external;
+
+/**
+ * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.interfaces.external)
+ * Created by: oloo
+ * On: 19/09/2024. 19:46
+ * Description:
+ **/
+public class frontEndHandler {
+}
