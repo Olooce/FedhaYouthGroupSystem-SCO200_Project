222ec6da18e2c358c6328c7730c5c2046ff60ee8
Olooce <asirastephen9@gmail.com>
2024-09-21 20:00:02 +0300

    feat: Update accounting/AccountingClass.java
    added: accounting/AccountingClass.java

---
+++ src/main/java/ac/ku/oloo/businessLogic/accounting/AccountingClass.java
@@ -0,0 +1,10 @@
+package ac.ku.oloo.businessLogic.accounting;
+
+/**
+ * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.businessLogic.accounting)
+ * Created by: oloo
+ * On: 21/09/2024. 19:59
+ * Description: 
+ **/
+public class AccountingClass {
+}
