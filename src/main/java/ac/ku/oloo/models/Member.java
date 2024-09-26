4e74c48e3a9bb49f7fe08b0d4fb7c099fb8a6c75
Olooce <asirastephen9@gmail.com>
2024-09-26 15:40:04 +0300

    chore: Update models/Member.java
    modified: models/Member.java

--- src/main/java/ac/ku/oloo/models/Member.java
+++ src/main/java/ac/ku/oloo/models/Member.java
@@ -1,10 +1,151 @@
 package ac.ku.oloo.models;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 /**
  * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.models)
  * Created by: oloo
  * On: 26/09/2024. 15:29
- * Description:
+ * Description: Model class representing the members table.
  **/
 public class Member {
+
+    private int memberId;
+    private String honorific;
+    private String surname;
+    private String firstName;
+    private String otherName;
+    private Date dateOfBirth;
+    private String idNumber;
+    private String idType;
+    private String taxId;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String status;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateModified;
+
+    // Getters and Setters
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getHonorific() {
+        return honorific;
+    }
+
+    public void setHonorific(String honorific) {
+        this.honorific = honorific;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getOtherName() {
+        return otherName;
+    }
+
+    public void setOtherName(String otherName) {
+        this.otherName = otherName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getIdType() {
+        return idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(LocalDateTime dateModified) {
+        this.dateModified = dateModified;
+    }
 }
