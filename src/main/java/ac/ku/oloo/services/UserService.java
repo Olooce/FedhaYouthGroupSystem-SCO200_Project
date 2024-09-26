dddf10447941203eb4063453b27441c5271d4308
Olooce <asirastephen9@gmail.com>
2024-09-26 21:40:03 +0300

    feat: Update services/UserService.java
    added: services/UserService.java

---
+++ src/main/java/ac/ku/oloo/services/UserService.java
@@ -0,0 +1,10 @@
+package ac.ku.oloo.services;
+
+/**
+ * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.services)
+ * Created by: oloo
+ * On: 26/09/2024. 21:39
+ * Description: 
+ **/
+public class UserService {
+}
