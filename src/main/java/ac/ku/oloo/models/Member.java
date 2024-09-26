ba5f01b9820090a567d1d7222753cbde9478e6e0
Olooce <asirastephen9@gmail.com>
2024-09-26 15:30:06 +0300

    feat: Update models/Member.java, services/MemberService.java
    added: models/Member.java
    added: services/MemberService.java

---
+++ src/main/java/ac/ku/oloo/models/Member.java
@@ -0,0 +1,10 @@
+package ac.ku.oloo.models;
+
+/**
+ * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.models)
+ * Created by: oloo
+ * On: 26/09/2024. 15:29
+ * Description:
+ **/
+public class Member {
+}
---
+++ src/main/java/ac/ku/oloo/services/MemberService.java
@@ -0,0 +1,15 @@
+package ac.ku.oloo.services;
+
+import java.lang.reflect.Member;
+
+/**
+ * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.services)
+ * Created by: oloo
+ * On: 26/09/2024. 15:29
+ * Description:
+ **/
+public class MemberService {
+    public void createMember(Member member) {
+
+    }
+}
