55fb8c32503434920800c25de3e71f4d355427fd
Olooce <asirastephen9@gmail.com>
2024-09-26 15:50:05 +0300

    chore: Update services/MemberService.java
    modified: services/MemberService.java

--- src/main/java/ac/ku/oloo/services/MemberService.java
+++ src/main/java/ac/ku/oloo/services/MemberService.java
@@ -1,6 +1,8 @@
 package ac.ku.oloo.services;
 
-import java.lang.reflect.Member;
+import ac.ku.oloo.models.Member;
+import java.sql.SQLException;
+import static ac.ku.oloo.utils.databaseUtil.QueryExecutor.*;
 
 /**
  * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.services)
@@ -9,7 +11,28 @@ import java.lang.reflect.Member;
  * Description:
  **/
 public class MemberService {
-    public void createMember(Member member) {
+    public void createMember(Member member) throws SQLException {
+        String sql = "INSERT INTO members (honorific, surname, first_name, other_name, date_of_birth, id_number, id_type, tax_id, email, phone_number, address, status, date_created, date_modified) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+        
+        Integer generatedMemberId = executeInsert(sql, rs -> {if (rs.next()) {
+                    return rs.getInt(1);
+                }
+                    return null;},
+                member.getHonorific(),
+                member.getSurname(),
+                member.getFirstName(),
+                member.getOtherName(),
+                member.getDateOfBirth(),
+                member.getIdNumber(),
+                member.getIdType(),
+                member.getTaxId(),
+                member.getEmail(),
+                member.getPhoneNumber(),
+                member.getAddress(),
+                member.getStatus());
 
+        member.setMemberId(generatedMemberId);
     }
+
 }
