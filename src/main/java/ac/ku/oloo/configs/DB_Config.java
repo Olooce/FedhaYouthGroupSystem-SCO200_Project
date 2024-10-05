a8d9b1ba4be90f650b84fb91b2817e43d7b62435
Olooce <asirastephen9@gmail.com>
2024-10-05 21:20:01 +0300

    feat: Update lib/DB-Initializer-1.0.jar, configs/DB_Config.java
    added: lib/DB-Initializer-1.0.jar
    added: configs/DB_Config.java

---
+++ src/main/java/ac/ku/oloo/configs/DB_Config.java
@@ -0,0 +1,10 @@
+package ac.ku.oloo.configs;
+
+/**
+ * FedhaYouthGroupSystem-SCO200_Project (ac.ku.oloo.configs)
+ * Created by: oloo
+ * On: 05/10/2024. 21:18
+ * Description: 
+ **/
+public class DB_Config {
+}
